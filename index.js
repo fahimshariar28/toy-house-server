59f207b88f7fad7203dbf9069bda5bd8f28a9672
Fahim Shariar <fahimshariar28@gmail.com>
2023-05-18 01:24:06 +0600

    get categories from mongodb

--- index.js
+++ index.js
@@ -23,11 +23,26 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const toyCollection = client.db("toyCollection").collection("toys");
+    const categoryCollection = client
+      .db("toyCollection")
+      .collection("categories");
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+
+    app.get("/categories", async (req, res) => {
+      const cursor = categoryCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/toys", async (req, res) => {
+      const cursor = toyCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
